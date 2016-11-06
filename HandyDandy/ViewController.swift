773494e93746331e59fe6b3160ec4ed163fc5c92
Mugur <mugur.moscaliuc@gmail.com>
2016-11-06 11:56:57 +0200

    Initial Commit

---
+++ HandyDandy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandyDandy
+//
+//  Created by yakis on 06/11/2016.
+//  Copyright © 2016 com.yakis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
