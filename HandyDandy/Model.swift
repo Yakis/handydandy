b70cddede69a09c096fc317e9c9980a224b9d473
Mugur <mugur.moscaliuc@gmail.com>
2016-11-06 12:41:29 +0200

    Model class added

---
+++ HandyDandy/Model.swift
@@ -0,0 +1,26 @@
+//
+//  Model.swift
+//  HandyDandy
+//
+//  Created by yakis on 06/11/2016.
+//  Copyright © 2016 com.yakis. All rights reserved.
+//
+
+import Foundation
+
+
+class Model: NSObject {
+
+    
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+        
+    }
+    
+    
+    
+    
+    
+}
